00db5429a81e52a3c4c70b7d2efffdf8f3655f81
name <ghazaryan.artur1@gmail.com>
2020-06-08 12:08:19 +0400

    smal fix

--- system/View.php
+++ system/View.php
@@ -2,16 +2,11 @@
 namespace system;
 
 class View {
-	function __construct (){
-	}
-
 
 	public function render($view_file){
-		if (file_exists("views/".$view_file.".php")){
-			session_start();
-			$_SESSION['text1'] = $this->firstArrg;
-			$_SESSION['text2'] = $this->secondArrg;
-			include "views/".$view_file.".php";
+		if (file_exists("views/pattern.php")){
+
+			include "views/pattern.php";
 		}
 	}
 }
--- views/settings.php
+++ views/settings.php
@@ -1,8 +1,4 @@
 <h1>helllo</h1>
-<?php if(isset($_SESSION['text1'])) : ?>
-    <p><?=$_SESSION['text1']?></p>
-<?php endif ?>
 
-<?php if(isset($_SESSION['text2'])) : ?>
-    <p><?=$_SESSION['text2']?></p>
-<?php endif ?>
+<p><?= $this->firstArrg?></p>
+<p><?= $this->secondArrg?></p>
